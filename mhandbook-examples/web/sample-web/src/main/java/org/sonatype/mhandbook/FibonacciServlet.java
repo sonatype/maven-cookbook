309e21d483ac72cea9b1aa34c7a7f6a5c778fd41
Tim O'Brien <tobrien@discursive.com>
2009-11-28 19:32:09 -0600

    Added a chapter on web development and a Jetty recipe

---
+++ mhandbook-examples/web/sample-web/src/main/java/org/sonatype/mhandbook/FibonacciServlet.java
@@ -0,0 +1,25 @@
+package org.sonatype.mhandbook;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FibonacciServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		int index = Integer.parseInt(req.getParameter("index"));
+		resp.getWriter().write( fib(index) + "" );
+		resp.getWriter().flush();
+		resp.getWriter().close();
+	}
+
+    public long fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n-1) + fib(n-2);
+    }
+}
